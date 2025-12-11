d033d2301bed34abc56e817861345782b4db9d87
Benjamin Poirier <benjamin.poirier@gmail.com>
2025-12-11 17:31:47 -0500

    compat.h: Add boilerplate
    Specify the license and add the usual include guards.

--- src/compat.h
+++ src/compat.h
@@ -1,4 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef COMPAT_H
+#define COMPAT_H
+
 // pre v6.16 compat
 #ifndef timer_container_of
 #define timer_container_of from_timer
 #endif
+
+#endif /* COMPAT_H */
